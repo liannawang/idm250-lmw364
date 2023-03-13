adb7b038bf15650a111ffdd77ee23a042ac44795
liannawang <91167762+liannawang@users.noreply.github.com>
2023-03-13 17:00:47 -0400

    404 and sample page

--- wp-content/themes/elixr theme/page.php
+++ wp-content/themes/elixr theme/page.php
@@ -4,7 +4,6 @@
 <!-- Gets the title of the page and puts it on the page -->
 <h1> <?php echo get_the_title(); ?> </h1>
 
-
-<h1> This is a page.</h1>
+<?php get_template_part('components/content'); ?>
 
 <?php get_footer(); ?>
